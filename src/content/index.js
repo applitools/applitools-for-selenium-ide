3ea8dd75861e5d48471faa303f3528b51b0cee46
Tomer <tomer@corevo.io>
2018-02-07 17:44:02 +0200

    fixed issue with setviewport size, where the size was reported as 0x0

--- src/content/index.js
+++ src/content/index.js
@@ -2,19 +2,46 @@ import browser from "webextension-polyfill";
 
 function sizeMessenger(request, sender, sendResponse) {
   if (request.compensateSize) {
+    const [width, height] = getInnerSize();
     sendResponse({
-      width: window.outerWidth - window.innerWidth,
-      height: window.outerHeight - window.innerHeight
+      width: window.outerWidth - width,
+      height: window.outerHeight - height
     });
   } else if (request.getSize) {
     setTimeout(() => {
+      const [width, height] = getInnerSize();
       sendResponse({
-        width: window.innerWidth,
-        height: window.innerHeight
+        width,
+        height
       });
     }, 100);
     return true;
   }
 }
+
+function getInnerSize() {
+  let height, width;
+  if (window.innerHeight) {
+    height = window.innerHeight;
+  } else if (document.documentElement && document.documentElement.clientHeight) {
+    height = document.documentElement.clientHeight;
+  } else {
+    let b = document.getElementsByTagName("body")[0];
+    if (b.clientHeight) {
+      height = b.clientHeight;
+    }
+  }
+  if (window.innerWidth) {
+    width = window.innerWidth;
+  } else if (document.documentElement && document.documentElement.clientWidth) {
+    width = document.documentElement.clientWidth;
+  } else {
+    let b = document.getElementsByTagName("body")[0];
+    if (b.clientWidth) { width = b.clientWidth;
+    }
+  }
+  return [width, height];
+}
+
 
 browser.runtime.onMessage.addListener(sizeMessenger);
