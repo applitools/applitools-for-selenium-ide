575a6438bfd09fdb108ddcb68c59bbae22400646
Tomer <tomer@corevo.io>
2018-02-13 17:30:00 +0200

    override selenium ide extension id from settings

--- src/IO/message-port.js
+++ src/IO/message-port.js
@@ -1,9 +1,13 @@
 import browser from "webextension-polyfill";
 
 export function sendMessage(payload) {
-  return browser.runtime.sendMessage(getId(), payload);
+  return getId().then(id => (
+    browser.runtime.sendMessage(id, payload)
+  ));
 }
 
 function getId() {
-  return process.env.SIDE_ID;
+  return browser.storage.local.get(["seideId"]).then(results => (
+    results.seideId ? results.seideId : process.env.SIDE_ID
+  ));
 }
