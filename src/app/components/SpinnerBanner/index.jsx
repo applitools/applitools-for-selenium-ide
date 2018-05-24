e73a5e70f3ba6f5fe84c751fbec9fe030b77090d
Tomer <tomer@corevo.io>
2018-05-24 13:11:21 +0300

    Image instead of spinner

--- src/app/components/SpinnerBanner/index.jsx
+++ src/app/components/SpinnerBanner/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import tick from "../../assets/images/tick.png";
+import warning from "../../assets/images/warning.png";
 import "./style.css";
 
 export const SpinnerStates = {
@@ -8,16 +10,25 @@ export const SpinnerStates = {
   ERROR: "error"
 };
 
+const StatusImages = {
+  [SpinnerStates.SUCCESS]: tick,
+  [SpinnerStates.ERROR]: warning
+};
+
 export default class SpinnerBanner extends React.Component {
   static propTypes = {
     children: PropTypes.node,
+    spin: PropTypes.bool,
     state: PropTypes.oneOf(Object.values(SpinnerStates)).isRequired,
     style: PropTypes.object
+  };
+  static defaultProps = {
+    spin: true
   };
   render() {
     return (
       <div className={classNames("banner", this.props.state)} style={this.props.style}>
-        <span className="loader"></span><span>{this.props.children}</span>
+        {this.props.spin ? <span className="loader"></span> : <img width="32px" src={StatusImages[this.props.state]} />}<span>{this.props.children}</span>
       </div>
     );
   }
