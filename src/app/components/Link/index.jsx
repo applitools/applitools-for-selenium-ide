c4b46f0bccaf8a4264a930cf98b40498d4ff2c61
Tomer <tomer@corevo.io>
2018-05-24 11:37:32 +0300

    Link component

---
+++ src/app/components/Link/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// as we can't open target="_self" links in extention popup we'll use a component to always add target="_blank"
+export default class Link extends React.Component {
+  render() {
+    return (
+      <a target="_blank" {...this.props} />
+    );
+  }
+}
