d43a06d0b247a83a461c511b957a95e8134119a4
Tomer <tomer@corevo.io>
2018-05-24 11:06:41 +0300

    display the banner on top

--- src/app/components/DisconnectBanner/index.jsx
+++ src/app/components/DisconnectBanner/index.jsx
@@ -5,13 +5,9 @@ export default class DiconnectBanner extends React.Component {
   render() {
     return (
       <SpinnerBanner state={SpinnerStates.ERROR} style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        height: "40px"
+        height: "75px"
       }}>
-        Trying to connect with Selenium IDE...<br />Please make sure the Selenium IDE extension window is open.
+        Trying to connect with Selenium IDE...
       </SpinnerBanner>
     );
   }
--- src/app/containers/Panel/index.jsx
+++ src/app/containers/Panel/index.jsx
@@ -2,6 +2,7 @@ import browser from "webextension-polyfill";
 import React from "react";
 import Modes from "../../../commons/modes";
 import RecordToolbar from "../RecordToolbar";
+import PlaybackBanner from "../../components/PlaybackBanner";
 import DisconnectBanner from "../../components/DisconnectBanner";
 import "../../styles/app.css";
 
@@ -40,12 +41,16 @@ export default class Panel extends React.Component {
   }
   render() {
     return (
-      <div style={{
-        margin: "5px 10px"
-      }}>
+      <div>
+        {this.state.mode === Modes.DISCONNECTED && <DisconnectBanner />}
+        <div className="container">
+          {this.state.mode === Modes.DISCONNECTED && <p>
+            Please make sure the Selenium IDE window is open. For more information visit <a href="https://applitools.com">https://applitools.com</a>
+          </p>}
+        </div>
         <div style= {{
-          height: "30px",
-          margin: "5px 0"
+          height: "20px",
+          margin: "5px 10px"
         }}>
           <div style={{
             display: "flex",
@@ -69,9 +74,20 @@ export default class Panel extends React.Component {
         </div>
         {this.state.mode === Modes.NORMAL && <div>Successfully connected to Selenium IDE. More options will be available when running or recording tests.</div>}
         {this.state.mode === Modes.SETUP && <div>Your Eyes account information is not properly set up. Please go to <a href="#" onClick={this.openOptionsPage}>options</a> to configure your account details.</div>}
-        {this.state.mode === Modes.PLAYBACK && <div>{this.state.playback.message}</div>}
+        {
+          this.state.mode === Modes.PLAYBACK &&
+            <PlaybackBanner
+              testName={this.state.playback.testName}
+              startTime={new Date(this.state.playback.startTime)}
+              hasFailed={this.state.playback.hasFailed}
+              batchName={this.state.playback.batchName}
+              appName={this.state.playback.appName}
+              eyesServer={this.state.playback.eyesServer}
+              environment={this.state.playback.environment}
+              branch={this.state.playback.branch}
+            />
+        }
         {this.state.mode === Modes.RECORD && <RecordToolbar />}
-        {this.state.mode === Modes.DISCONNECTED && <DisconnectBanner />}
       </div>
     );
   }
