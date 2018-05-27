366b3091c52c968b9168fe3973b35dbdd7c2fe39
Tomer <tomer@corevo.io>
2018-05-27 14:41:27 +0300

    button list component

---
+++ src/app/components/ButtonList/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.css";
+
+export default class ButtonList extends React.Component {
+  static propTypes = {
+    items: PropTypes.array.isRequired,
+    label: PropTypes.string,
+    onClick: PropTypes.func.isRequired
+  };
+  render() {
+    return (
+      <ul className="buttons">
+        {this.props.items.map((item) => (
+          <ListButton key={item} name={item} label={this.props.label} onClick={this.props.onClick} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+class ListButton extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    onClick: PropTypes.func.isRequired
+  };
+  render() {
+    return (
+      <li tabIndex="0" onClick={this.props.onClick}>
+        <button tabIndex="-1">{this.props.name}</button>
+        {this.props.label && <a>{this.props.label}</a>}
+      </li>
+    );
+  }
+}
