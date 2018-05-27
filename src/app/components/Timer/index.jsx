80548461bc4b3c9b91ebd80ff0cc3574a8c984f5
Tomer <tomer@corevo.io>
2018-05-27 16:48:08 +0300

    fixed a bug where the timer would only render a second later

--- src/app/components/Timer/index.jsx
+++ src/app/components/Timer/index.jsx
@@ -4,20 +4,22 @@ import PropTypes from "prop-types";
 export default class Timer extends React.Component {
   constructor(props) {
     super(props);
+    const parseTime = (time, placeholder) => {
+      if (time) {
+        // This fails when DST is on, but we calculate under 1 hour, thus its fine.
+        const diff = new Date(new Date() - time);
+        return `${diff.getMinutes()} min, ${diff.getSeconds()} seconds`;
+      } else {
+        return placeholder || "idle";
+      }
+    };
     this.state = {
+      time: parseTime(props.time, props.placeholder)
     };
     this.intervalDisposer = setInterval(() => {
-      if (this.props.time) {
-        // This fails when DST is on, but we calculate under 1 hour, thus its fine.
-        const diff = new Date(new Date() - this.props.time);
-        this.setState({
-          time: `${diff.getMinutes()} min, ${diff.getSeconds()} seconds`
-        });
-      } else {
-        this.setState({
-          time: this.props.placeholder || "idle"
-        });
-      }
+      this.setState({
+        time: parseTime(this.props.time, this.props.placeholder)
+      });
     }, 1000);
   }
   static propTypes = {
