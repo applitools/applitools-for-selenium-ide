058670180393bbca347530c2b3a6e8c158a81f44
Tomer <tomer@corevo.io>
2018-05-28 13:09:31 +0300

    added link to applitools test manager in normal view

--- src/app/containers/Normal/index.jsx
+++ src/app/containers/Normal/index.jsx
@@ -3,8 +3,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import Checkbox from "../../components/Checkbox";
 import MoreInfo from "../../components/MoreInfo";
+import Link from "../../components/Link";
+import { DEFAULT_SERVER } from "../../../commons/api.js";
 
 export default class Normal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    browser.storage.local.get(["eyesServer"]).then(({eyesServer}) => {
+      this.setState({
+        eyesServer
+      });
+    });
+  }
   static propTypes = {
     disableVisualCheckpoints: PropTypes.bool.isRequired,
     visualCheckpointsChanged: PropTypes.func.isRequired
@@ -31,6 +42,11 @@ export default class Normal extends React.Component {
         <a href="#" onClick={this.openOptionsPage} style={{
           marginLeft: "30px"
         }}>Open settings</a>
+        <Link href={(new URL("/app/test-results/", this.state.eyesServer || DEFAULT_SERVER)).href} style={{
+          display: "block",
+          marginTop: "5px",
+          marginLeft: "30px"
+        }}>Open test manager</Link>
         <footer>
           <p>More options will be available when running or recording tests. <MoreInfo /></p>
         </footer>
