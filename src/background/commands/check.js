b037dd3b98c0a647421fecbc9792be509f2a3c28
Tomer <tomer@corevo.io>
2018-05-28 13:21:39 +0300

    set playback state before the first check

--- src/background/commands/check.js
+++ src/background/commands/check.js
@@ -1,23 +1,25 @@
 import browser from "webextension-polyfill";
+import Modes from "../../commons/modes";
 import { sendMessage } from "../../IO/message-port";
 import { getScreenshot, getRegionScreenshot, isRegionInViewport, scrollTo } from "../utils/screenshot";
 import { getEyes, closeEyes } from "../utils/eyes";
+import { getExternalState, setExternalState } from "../external-state";
+import { parseEnvironment } from "../utils/parsers";
 import ideLogger from "../utils/ide-logger";
 
 export function checkWindow(runId, testId, commandId, tabId, windowId, stepName, viewport, forceFullPageScreenshot = true, removeScrollBars = true) {
   return new Promise((resolve, reject) => {
     getEyes(`${runId}${testId}`).then(eyes => {
-      if (!eyes.didSetViewportSize) {
-        ideLogger.warn("a visual check was called without setting a viewport size, results may be inconsistent");
-      }
-      eyes.commands.push(commandId);
-      eyes.setViewportSize(viewport);
-      getScreenshot(tabId, windowId, forceFullPageScreenshot, removeScrollBars, viewport).then((image) => {
-        const image64 = image.replace("data:image/png;base64,", "");
-        return eyes.checkImage(image64, stepName);
-      }).then((imageResult) => {
-        return imageResult.asExpected ? resolve(true) : resolve({ status: "undetermined" });
-      }).catch(reject);
+      preCheck(eyes, viewport).then(() => {
+        eyes.commands.push(commandId);
+        eyes.setViewportSize(viewport);
+        getScreenshot(tabId, windowId, forceFullPageScreenshot, removeScrollBars, viewport).then((image) => {
+          const image64 = image.replace("data:image/png;base64,", "");
+          return eyes.checkImage(image64, stepName);
+        }).then((imageResult) => {
+          return imageResult.asExpected ? resolve(true) : resolve({ status: "undetermined" });
+        }).catch(reject);
+      });
     }).catch(reject);
   });
 }
@@ -26,28 +28,27 @@ export function checkRegion(runId, testId, commandId, tabId, windowId, region, s
   if (!region || !region.left || !region.top || !region.width || !region.height) return Promise.reject("Invalid region. Region should be x: [number], y: [number], width: [number], height: [number]");
   return new Promise((resolve, reject) => {
     getEyes(`${runId}${testId}`).then(eyes => {
-      if (!eyes.didSetViewportSize) {
-        ideLogger.warn("a visual check was called without setting a viewport size, results may be inconsistent");
-      }
-      eyes.commands.push(commandId);
-      eyes.setViewportSize(viewport);
-      let scrollToTopTarget = region.top - 100;
-      if (scrollToTopTarget < 0) {
-        scrollToTopTarget = 0;
-      } else {
-        region.top = 100;
-      }
-      scrollTo(tabId, region.left, scrollToTopTarget).then(() => {
-        if (isRegionInViewport(region, viewport)) {
-          getRegionScreenshot(tabId, windowId, region, removeScrollBars, viewport).then((image) => {
-            const image64 = image.replace("data:image/png;base64,", "");
-            return eyes.checkImage(image64, stepName);
-          }).then((imageResult) => {
-            return imageResult.asExpected ? resolve(true) : resolve({ status: "undetermined" });
-          }).catch(reject);
+      preCheck(eyes, viewport).then(() => {
+        eyes.commands.push(commandId);
+        eyes.setViewportSize(viewport);
+        let scrollToTopTarget = region.top - 100;
+        if (scrollToTopTarget < 0) {
+          scrollToTopTarget = 0;
         } else {
-          reject(new Error("Region is out of bounds, try setting the viewport size to a bigger one."));
+          region.top = 100;
         }
+        scrollTo(tabId, region.left, scrollToTopTarget).then(() => {
+          if (isRegionInViewport(region, viewport)) {
+            getRegionScreenshot(tabId, windowId, region, removeScrollBars, viewport).then((image) => {
+              const image64 = image.replace("data:image/png;base64,", "");
+              return eyes.checkImage(image64, stepName);
+            }).then((imageResult) => {
+              return imageResult.asExpected ? resolve(true) : resolve({ status: "undetermined" });
+            }).catch(reject);
+          } else {
+            reject(new Error("Region is out of bounds, try setting the viewport size to a bigger one."));
+          }
+        });
       });
     }).catch(reject);
   });
@@ -56,25 +57,24 @@ export function checkRegion(runId, testId, commandId, tabId, windowId, region, s
 export function checkElement(runId, testId, commandId, tabId, windowId, elementXPath, stepName, viewport, removeScrollBars = false) {
   return new Promise((resolve, reject) => {
     getEyes(`${runId}${testId}`).then(eyes => {
-      if (!eyes.didSetViewportSize) {
-        ideLogger.warn("a visual check was called without setting a viewport size, results may be inconsistent");
-      }
-      eyes.commands.push(commandId);
-      eyes.setViewportSize(viewport);
-      browser.tabs.sendMessage(tabId, {
-        getElementRect: true,
-        path: elementXPath
-      }).then((rect) => {
-        if (isRegionInViewport(rect, viewport)) {
-          getRegionScreenshot(tabId, windowId, rect, removeScrollBars, viewport).then((image) => {
-            const image64 = image.replace("data:image/png;base64,", "");
-            return eyes.checkImage(image64, stepName);
-          }).then((imageResult) => {
-            return imageResult.asExpected ? resolve(true) : resolve({ status: "undetermined" });
-          }).catch(reject);
-        } else {
-          reject(new Error("Element is out of bounds, try setting the viewport size to a bigger one."));
-        }
+      preCheck(eyes, viewport).then(() => {
+        eyes.commands.push(commandId);
+        eyes.setViewportSize(viewport);
+        browser.tabs.sendMessage(tabId, {
+          getElementRect: true,
+          path: elementXPath
+        }).then((rect) => {
+          if (isRegionInViewport(rect, viewport)) {
+            getRegionScreenshot(tabId, windowId, rect, removeScrollBars, viewport).then((image) => {
+              const image64 = image.replace("data:image/png;base64,", "");
+              return eyes.checkImage(image64, stepName);
+            }).then((imageResult) => {
+              return imageResult.asExpected ? resolve(true) : resolve({ status: "undetermined" });
+            }).catch(reject);
+          } else {
+            reject(new Error("Element is out of bounds, try setting the viewport size to a bigger one."));
+          }
+        });
       });
     }).catch(reject);
   });
@@ -99,4 +99,35 @@ export function endTest(id) {
         : { error: `There are visual tests failures,\nresults: ${results.appUrls.session}` };
     });
   });
+}
+
+function preCheck(eyes, viewport) {
+  if (!eyes.didSetViewportSize) {
+    ideLogger.warn("a visual check was called without setting a viewport size, results may be inconsistent");
+  }
+  if (getExternalState().mode !== Modes.PLAYBACK) {
+    return browser.storage.local.get(["apiKey", "branch", "parentBranch", "eyesServer"]).then(({ branch, parentBranch, eyesServer }) => {
+      let notification = `connecting to ${eyesServer ? eyesServer : "public eyes"}`;
+      if (branch) {
+        notification += `, running using branch ${branch}${parentBranch ? " and parent branch " + parentBranch : ""}`;
+      }
+      return ideLogger.log(notification).then(() => {
+        return setExternalState({
+          mode: Modes.PLAYBACK,
+          playback: {
+            testName: eyes._testName,
+            startTime: (new Date()).toString(),
+            hasFailed: false,
+            batchName: eyes._batch.name || eyes._testName,
+            appName: eyes._appName,
+            eyesServer,
+            environment: parseEnvironment(navigator.userAgent, viewport),
+            branch
+          }
+        });
+      });
+    });
+  } else {
+    return Promise.resolve();
+  }
 }
