9d2bda4492c7166aeedc3ef01f1abfb4f2d38c5f
Tomer <tomer@corevo.io>
2018-05-29 12:04:40 +0300

    load and save options

--- src/options/containers/Root/index.jsx
+++ src/options/containers/Root/index.jsx
@@ -1,3 +1,4 @@
+import browser from "webextension-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import TabBar from "../../../commons/components/TabBar";
@@ -17,22 +18,72 @@ class Options extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tab: Tabs.TESTS
+      tab: Tabs.TESTS,
+      disableVisualCheckpoints: false,
+      branch: "",
+      parentBranch: "",
+      apiKey: "",
+      eyesServer: "",
+      seideId: ""
     };
     this.tabs = Object.values(Tabs);
-    this.handleTabChanged = this.handleTabChanged.bind(this);
+    this.handleTabChange = this.handleTabChange.bind(this);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.saveOptions = this.saveOptions.bind(this);
+    browser.storage.local.get([
+      "disableVisualCheckpoints",
+      "apiKey",
+      "eyesServer",
+      "branch",
+      "parentBranch",
+      "seideId"]
+    ).then(({disableVisualCheckpoints, apiKey, eyesServer, branch, parentBranch, seideId}) => {
+      this.setState({
+        disableVisualCheckpoints,
+        apiKey,
+        eyesServer,
+        branch,
+        parentBranch,
+        seideId
+      });
+    });
   }
-  handleTabChanged(tab) {
+  handleTabChange(tab) {
     this.setState({
       tab
+    });
+  }
+  handleCheckboxChange(e) {
+    this.setState({
+      disableVisualCheckpoints: e.target.checked
+    });
+  }
+  handleInputChange(name, value) {
+    this.setState({
+      [name]: value
+    });
+  }
+  saveOptions() {
+    browser.storage.local.set({
+      disableVisualCheckpoints: this.state.disableVisualCheckpoints,
+      apiKey: this.state.apiKey,
+      eyesServer: this.state.eyesServer,
+      branch: this.state.branch,
+      parentBranch: this.state.parentBranch,
+      seideId: this.state.seideId
+    }).then(() => {
+      browser.runtime.sendMessage({
+        optionsUpdated: true
+      });
+      window.close();
     });
   }
   render() {
     return (
       <div>
-        <TabBar tabs={this.tabs} tabChanged={this.handleTabChanged} />
+        <TabBar tabs={this.tabs} tabChanged={this.handleTabChange} />
         <form onSubmit={(e) => { e.preventDefault(); }}>
-          <div className="form-content">
+          <div className="form-contents">
             {this.state.tab === Tabs.TESTS &&
             <React.Fragment>
               <Checkbox
@@ -40,21 +91,23 @@ class Options extends React.Component {
                 className="checkbox"
                 name="disable-checks"
                 label="Disable visual checkpoints"
+                checked={this.state.disableVisualCheckpoints}
+                onChange={this.handleCheckboxChange}
               />
-              <Input name="branch" label="branch name" placeholder="default" />
-              <Input name="parentBranch" label="parent branch name" />
+              <Input name="branch" label="branch name" placeholder="default" value={this.state.branch} onChange={this.handleInputChange.bind(this,  "branch")} />
+              <Input name="parentBranch" label="parent branch name" value={this.state.parentBranch} onChange={this.handleInputChange.bind(this, "parentBranch")} />
             </React.Fragment>}
             {this.state.tab === Tabs.ACCOUNT &&
             <React.Fragment>
-              <Input name="apiKey" label="api key" />
-              <Input name="serverUrl" label="server url" placeholder="https://eyes.applitools.com" />
+              <Input name="apiKey" label="api key" value={this.state.apiKey} onChange={this.handleInputChange.bind(this, "apiKey")} />
+              <Input name="serverUrl" label="server url" placeholder="https://eyes.applitools.com" value={this.state.eyesServer} onChange={this.handleInputChange.bind(this, "eyesServer")} />
             </React.Fragment>}
             {this.state.tab === Tabs.ADVANCED &&
             <React.Fragment>
-              <Input name="sideId" label="ide extension id" />
+              <Input name="seideId" label="ide extension id" value={this.state.seideId} onChange={this.handleInputChange.bind(this, "seideId")} />
             </React.Fragment>}
           </div>
-          <FlatButton type="submit">Confirm</FlatButton>
+          <FlatButton type="submit" onClick={this.saveOptions}>Confirm</FlatButton>
           <div style={{clear: "both"}}></div>
         </form>
       </div>
