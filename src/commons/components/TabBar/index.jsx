b3bdc2a0f8a86bc2c6a3766619f593db550b1e2b
Tomer <tomer@corevo.io>
2018-05-29 11:27:10 +0300

    TabBar component

---
+++ src/commons/components/TabBar/index.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import "./style.css";
+
+export default class TabBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: props.defaultTab ? {
+        tab: props.defaultTab,
+        index: props.tabs.indexOf(props.defaultTab)
+      } : {
+        tab: props.tabs[0],
+        index: 0
+      }
+    };
+  }
+  static propTypes = {
+    tabs: PropTypes.array.isRequired,
+    defaultTab: PropTypes.string,
+    tabWidth: PropTypes.number,
+    tabChanged: PropTypes.func
+  };
+  static defaultProps = {
+    tabWidth: 80
+  };
+  handleClick(tab, index) {
+    if (tab !== this.state.activeTab.tab) {
+      this.setState({
+        activeTab: { tab, index }
+      });
+      if (this.props.tabChanged) this.props.tabChanged(tab);
+    }
+  }
+  render() {
+    return (
+      <div className="tabbar">
+        <ul ref={(node) => {
+          if (node) {
+            const tabPadding = Math.round(node.getBoundingClientRect().width / this.props.tabs.length - this.props.tabWidth);
+            if (tabPadding !== this.state.tabPadding) {
+              this.setState({ tabPadding });
+            }
+          }
+        }}>
+          {this.props.tabs.map((tab, index) => (
+            <li key={tab} className={classNames({active: tab === this.state.activeTab.tab})}>
+              <a onClick={this.handleClick.bind(this, tab, index)}>{tab}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="underline" style={{
+          transform: `translateX(${this.state.activeTab.index * (this.props.tabWidth + this.state.tabPadding) + this.state.tabPadding / 2}px)`,
+          width: `${this.props.tabWidth}px`
+        }}></div>
+      </div>
+    );
+  }
+}
