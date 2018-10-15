9c3481194a928bb546e7dfb3dc3bc8d6e96b5c9d
David Haeffner <dhaeffner@gmail.com>
2018-10-15 09:06:10 +0300

    Updated docs link to point to new tutorial

--- src/app/components/MoreInfo/index.jsx
+++ src/app/components/MoreInfo/index.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import Link from "../../../commons/components/Link";
 
 export default function MoreInfo() {
-  return <span>For more information visit <Link href="https://applitools.com/resources/tutorial/web/selenium-ide">https://applitools.com</Link></span>;
+  return <span>For more information visit <Link href="https://applitools.com/tutorials/selenium-ide.html">https://applitools.com</Link></span>;
 }
