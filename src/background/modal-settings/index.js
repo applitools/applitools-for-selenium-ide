90f8d30655eeda9478750d76a0d91d5ba42656ab
David Haeffner <dhaeffner@gmail.com>
2019-02-04 11:15:53 +0200

    Moved modal content into its own file

---
+++ src/background/modal-settings/index.js
@@ -0,0 +1,7 @@
+export const incompleteVisualGridSettings = {
+  message: `There are incomplete visual grid settings in the Eyes 
+    extension. Playback cannot be completed using the visual grid 
+    until the missing settings are fixed.`,
+  cancelLabel: 'Abort playback',
+  confirmLabel: 'Run without the grid',
+}
