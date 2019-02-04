f90c0577cf7933509f34176e61eeef65ca0e8d36
David Haeffner <dhaeffner@gmail.com>
2019-02-04 13:45:03 +0200

    Updated the body text for the invalid VG settings modal

--- src/background/modal-settings/index.js
+++ src/background/modal-settings/index.js
@@ -1,7 +1,6 @@
 export const incompleteVisualGridSettings = {
-  message: `There are incomplete visual grid settings in the Eyes 
-    extension. Playback cannot be completed using the visual grid 
-    until the missing settings are fixed.`,
+  message: `There are incomplete visual grid settings in the Eyes extension.
+  Playback using the visual grid cannot be completed until the needed settings are in-place.`,
   cancelLabel: 'Abort playback',
   confirmLabel: 'Run without the grid',
 }
