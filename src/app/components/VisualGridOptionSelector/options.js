ca462b25e328c76b4b7e17dfde71677731b5d139
David Haeffner <dhaeffner@gmail.com>
2019-05-06 16:23:09 +0300

    Added a deviceId object to translate from a given display name

--- src/app/components/VisualGridOptionSelector/options.js
+++ src/app/components/VisualGridOptionSelector/options.js
@@ -41,4 +41,36 @@ export const devices = [
   'Galaxy Note II',
 ]
 
+export const deviceIds = {
+  'iPhone 4': 'iPhone_4',
+  'iPhone 5/SE': 'iPhone_5SE',
+  'iPhone 6/7/8': 'iPhone_6_7_8',
+  'iPhone 6/7/8 Plus': 'iPhone_6_7_8_Plus',
+  'iPhone X': 'iPhone_X',
+  'BlackBerry Z30': 'BlackBerry_Z30',
+  'Nexus 4': 'Nexus_4',
+  'Nexus 5': 'Nexus_5',
+  'Nexus 5X': 'Nexus_5X',
+  'Nexus 6': 'Nexus_6',
+  'Nexus 6P': 'Nexus_6P',
+  'Pixel 2': 'Pixel_2',
+  'Pixel 2 XL': 'Pixel_2_XL',
+  'LG Optimus L70': 'LG_Optimus_L70',
+  'Nokia N9': 'Nokia_N9',
+  'Nokia Lumia 520': 'Nokia_Lumia_520',
+  'Microsoft Lumia 550': 'Microsoft_Lumia_550',
+  'Microsoft Lumia 950': 'Microsoft_Lumia_950',
+  'Galaxy S III': 'Galaxy_S3',
+  'Galaxy S5': 'Galaxy_S5',
+  'Kindle Fire HDX': 'Kindle_Fire_HDX',
+  'iPad Mini': 'iPad_Mini',
+  iPad: 'iPad',
+  'iPad Pro': 'iPad_Pro',
+  'Blackberry PlayBook': 'Blackberry_PlayBook',
+  'Nexus 10': 'Nexus_10',
+  'Nexus 7': 'Nexus_7',
+  'Galaxy Note 3': 'Galaxy_Note_3',
+  'Galaxy Note II': 'Galaxy_Note_2',
+}
+
 export const orientations = ['Portrait', 'Landscape']
